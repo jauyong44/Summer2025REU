2df491a07d979eef46032984a8c0afa93733409d
Shan <swpeck2004@gmail.com>
2025-07-01 15:31:40 -0600

    Fix losses

--- Local/BaseLocal.py
+++ Local/BaseLocal.py
@@ -14,7 +14,7 @@ class BaseLocal(LocalMethod):
         online_clients_list = kwargs['online_clients_list']
         nets_list = kwargs['nets_list']
         priloader_list = kwargs['priloader_list']
-        loss_data = kwargs.get('loss')
+        losses = kwargs.get('losses')
 
         for i in online_clients_list:
             self.train_net(i, nets_list[i], priloader_list[i])
