4cefed60a1a4430de844830285245a9be43b627f
Shan <swpeck2004@gmail.com>
2025-07-01 14:52:14 -0600

    Attempting to fix kwargs

--- Local/BaseLocal.py
+++ Local/BaseLocal.py
@@ -9,11 +9,16 @@ class BaseLocal(LocalMethod):
 
     def __init__(self, args, cfg):
         super(BaseLocal, self).__init__(args, cfg)
+        self.local_optimizer = get_optimizer(self.net, self.cfg.OPTIMIZER.type,
+                                             self.cfg.OPTIMIZER.local_train_lr,
+                                             self.cfg.OPTIMIZER.momentum, self.cfg.OPTIMIZER.weight_decay)
+        self.criterion = nn.CrossEntropyLoss()
 
-    def loc_update(self, loss, **kwargs):
+    def loc_update(self, **kwargs):
         online_clients_list = kwargs['online_clients_list']
         nets_list = kwargs['nets_list']
         priloader_list = kwargs['priloader_list']
+        loss_data = kwargs.get('loss')
 
         for i in online_clients_list:
             self.train_net(i, nets_list[i], priloader_list[i])
