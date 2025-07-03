805759d743ad469b32f7a6b58f481d4c2ab1d025
Shan <swpeck2004@gmail.com>
2025-07-03 14:00:05 -0600

    Update TRUFL.py

--- Methods/TRUFL.py
+++ Methods/TRUFL.py
@@ -1,4 +1,9 @@
+import copy
+import torch
+import torch.nn as nn
 from Methods.utils.meta_methods import FederatedMethod
+import numpy as np
+import random
 
 class TRUFL(FederatedMethod):
   NAME = 'TRUFL'
@@ -9,17 +14,17 @@ class TRUFL(FederatedMethod):
 
   def ini(self):
     super().ini()
+    # after global_net is initializaed, set it on the sever_model for diff calculation
+    if self.sever_model is not None:
+      self.sever_model.global_model_at_round_start = copy.deepcopy(self.global_net.state_dict())
 
   def local_update(self, priloader_list):
     total_clients = list(range(self.cfg.DATASET.parti_num))
     self.online_clients_list = self.random_state.choice(total_clients, self.online_num, replace=False).tolist()
 
     self.local_model.loc_update(online_clients_list=self.online_clients_list, nets_list=self.nets_list, global_net=self.global_net, 
-                                priloader_list=priloader_list)
+                                priloader_list=priloader_list, epoch_index=self.epoch_index)
 
   def sever_update(self, priloader_list):
-    self.aggregation_weight_list = self.sever_model.sever_update(fed_aggregation.self.fed_aggregation,
-                                                                 online_clients_list=self.online_clients_list,
-                                                                 priloader_list=priloader_list,
-                                                                 client_domain_list=self.client_domain_list,
-                                                                 global_net=self.global_net, nets_list=self.nets_list)
+    self.sever_model.sever_update(nets_list=self.nets_list, online_clients_list=self.online_clients_list,
+                                  epoch_index=self.epoch_index)
