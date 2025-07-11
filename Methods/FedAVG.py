9dca5e0aa0daf9e074ff7ae9a9849f22abe74901
Shan <swpeck2004@gmail.com>
2025-07-11 10:03:15 -0600

    attempt to fix BaseLocal issue

--- Methods/FedAVG.py
+++ Methods/FedAVG.py
@@ -1,5 +1,5 @@
 from Methods.utils.meta_methods import FederatedMethod
-
+from Local.utils import BaseLocal # Or wherever BaseLocal is located
 
 class FedAVG(FederatedMethod):
     NAME = 'FedAVG'
@@ -7,16 +7,30 @@ class FedAVG(FederatedMethod):
 
     def __init__(self, nets_list, client_domain_list, args, cfg):
         super(FedAVG, self).__init__(nets_list, client_domain_list, args, cfg)
+        self.local_model = BaseLocal(args=args, cfg=cfg)
 
     def ini(self):
         super().ini()
 
-    def local_update(self, priloader_list, losses=None):
-        total_clients = list(range(self.cfg.DATASET.parti_num))
+    total_clients = list(range(self.cfg.DATASET.parti_num))
         self.online_clients_list = self.random_state.choice(total_clients, self.online_num, replace=False).tolist()
 
-        self.local_model.loc_update(losses, online_clients_list=self.online_clients_list, nets_list=self.nets_list, global_net=self.global_net,
-                                    priloader_list=priloader_list)
+        # Loop through only the online clients to perform their local updates
+        for idx in self.online_clients_list:
+            self.nets_list[idx].train() # Set the client model to training mode
+
+            kwargs_for_loc_update = {
+                'online_clients_list': self.online_clients_list,
+                'nets_list': self.nets_list,
+                'priloader_list': priloader_list, # Pass the full list of loaders
+            }
+
+            # Only add 'losses' if it's not None
+            if losses is not None:
+                kwargs_for_loc_update['losses'] = losses
+
+
+            self.local_model.loc_update(**kwargs_for_loc_update)
 
     def sever_update(self, priloader_list):
         self.aggregation_weight_list = self.sever_model.sever_update(fed_aggregation=self.fed_aggregation,
