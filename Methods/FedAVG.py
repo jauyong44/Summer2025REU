5d3682c6677aff913d8eadd43dcf61860b99d521
Shan <swpeck2004@gmail.com>
2025-07-11 09:50:30 -0600

    set losses to none

--- Methods/FedAVG.py
+++ Methods/FedAVG.py
@@ -11,7 +11,7 @@ class FedAVG(FederatedMethod):
     def ini(self):
         super().ini()
 
-    def local_update(self, priloader_list, losses=none):
+    def local_update(self, priloader_list, losses=None):
         total_clients = list(range(self.cfg.DATASET.parti_num))
         self.online_clients_list = self.random_state.choice(total_clients, self.online_num, replace=False).tolist()
 
